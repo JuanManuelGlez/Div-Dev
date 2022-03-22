2bb60583981718a15ddee80661137701938ddcc3
EmilianoV-TEC <a01707035@itesm.mx>
2022-03-22 13:18:02 -0800

    Cambio

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const path = require('path');
 
 const app = express();
 
-app.set('views', 'views');
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: false}));
