1ce1706243c7284ec4de5ed22aa1a3ea1947dd86
Jorge Guerrero <jorge.guerrero.0023@gmail.com>
2022-03-24 16:15:47 -0600

    Development of Log in

--- util/is-auth.js
+++ util/is-auth.js
@@ -1,6 +1,6 @@
 module.exports=(request,response,next) =>{
     if (!request.session.isLoggedIn){
-        return response.status(403).redirect('/usuarios/login');
+        return response.status(403).redirect('usuario/login');
     }
     next();
 }
