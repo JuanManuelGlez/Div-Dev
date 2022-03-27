87503145b081815b597306a07281075cfbf8cf3b
unknown <a01707439@itesm.mx>
2022-03-27 15:20:43 -0600

    avance views

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
+
+const metricasruta = require('./routes/general.routes');
+app.use('/', metricasruta);
+app.listen(3000);
