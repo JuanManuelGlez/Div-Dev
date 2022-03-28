bde2c182d093ac2f957668b1cf10279b0ecc33fa
EmilianoV-TEC <a01707035@itesm.mx>
2022-03-28 15:44:08 -0800

    CU4 integración

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(session({
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'assets')));
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(bodyParser.json());
