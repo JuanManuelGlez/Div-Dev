d8dcf50cdaf1d4cdd81e288bec617dd16678d17a
EmilianoV-TEC <a01707035@itesm.mx>
2022-04-08 08:53:43 -0800

    Agraga validación de algunos formularios

--- public/js/script_nuevo_ticket.js
+++ public/js/script_nuevo_ticket.js
@@ -59,7 +59,7 @@ document.getElementById("select_tipo_incidencia").onchange = () =>
         let cont = 0;
         for(let pregunta of response.preguntas)
         {
-            preguntas.innerHTML += '<div class="row" style="padding: 15px 0px;"><div class="col-xxl-4"><label class="col-form-label text-dark">' + pregunta.Texto_Pregunta + ': </label></div> <div class="col"><input class="form-control" type="text" id="respuesta' + cont + '" name="respuesta' + cont + '">';
+            preguntas.innerHTML += '<div class="row" style="padding: 15px 0px;"><div class="col-xxl-4"><label class="col-form-label text-dark">' + pregunta.Texto_Pregunta + ': </label></div> <div class="col"><input class="form-control" type="text" id="respuesta' + cont + '" name="respuesta' + cont + '"maxlength="500">';
             preguntas.innerHTML += '<input type="hidden" id="pregunta' + cont + '" name="pregunta' + cont + '" value="' + pregunta.Id_Pregunta + '">';
             cont++;
         }
