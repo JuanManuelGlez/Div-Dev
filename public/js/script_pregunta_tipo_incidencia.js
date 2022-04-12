b378ab74c3db13ebbef0a14a20bb87e439f02ea3
Andres <andresacecar@gmail.com>
2022-04-12 00:18:09 -0500

    avance a tipo de incidencia select preguntas

--- public/js/script_pregunta_tipo_incidencia.js
+++ public/js/script_pregunta_tipo_incidencia.js
@@ -1,48 +1,37 @@
+$(document).ready(function () {
+    $('.form-select-label').selectpicker();
+});
 
-var cont = 0;
-add_pregunta = document.getElementById("add_pregunta");
-array_preguntas = [];
-contador = document.getElementById("contador_preguntas").value;
+$(document).on('keyup', '.form-select-label .bs-searchbox input', function (e) {
+    var input = e.target.value;
+    if(document.getElementsByClassName("no-results").length == 1)
+    {
+        document.getElementsByClassName("no-results")[0].innerHTML = "No se encontro la pregunta " + input;
 
-
-document.getElementById("Nueva_Pregunta").onclick = () =>
-{
-    if(cont == 0){
-        add_pregunta.innerHTML += '<br> <input type="text" id="pregunta_nueva_text'+ cont +'" name="pregunta_nueva_text'+ cont +'" placeholder="Escribe aqui...">';
-        add_pregunta.innerHTML += '<select onchange="getval('+cont+');" class="form-select" id="select_preguntas'+cont+'" name="select_preguntas'+cont+'" aria-label="Default select example"><option selected disabled>Preguntas:</option>';
-        for(let i= 0; i <= contador; i++){
-            document.getElementById('select_preguntas'+cont).options.add(new Option(document.getElementById('Preguntas'+i).value,document.getElementById('Preguntas'+i).value));
+        if(input.length > 30)
+        {
+            document.getElementsByClassName("no-results")[0].innerHTML += '<button type="button" disabled id="agregaPregunta">Agregar</button>';
         }
-        add_pregunta.innerHTML += '</select><br>';
-        cont++;
-
-    }else{
-        array_preguntas.push(document.getElementById('pregunta_nueva_text' + (cont-1)).value);
-
-        for(let i= 0; i < cont-1; i++){
-            array_preguntas[i] = document.getElementById('pregunta_nueva_text' + i).value;
+        else
+        {
+            document.getElementsByClassName("no-results")[0].innerHTML += '<button type="button" id="agregaPregunta">Agregar</button>';
         }
+        
+        document.getElementById("agregaPregunta").addEventListener('click', function (event) {
 
-        add_pregunta.innerHTML += '<br> <input type="text" id="pregunta_nueva_text'+ cont +'" name="pregunta_nueva_text'+ cont +'" placeholder="Escribe aqui..."> <br>';
+            let preguntas_actuales = document.querySelector('[data-id="select_preguntas"]').title.split(", ");
 
-        add_pregunta.innerHTML += '<select onchange="getval('+cont+');" class="form-select" id="select_preguntas'+cont+'" name="select_preguntas'+cont+'" aria-label="Default select example"><option selected disabled>Preguntas:</option>';
-        for(let i= 0; i <= contador; i++){
-            document.getElementById('select_preguntas'+cont).options.add(new Option(document.getElementById('Preguntas'+i).value, document.getElementById('Preguntas'+i).value));
-        }
-        add_pregunta.innerHTML += '</select><br>';
+            for(pregunta of preguntas_actuales)
+            {
+                document.getElementById(pregunta).setAttribute('selected', true);
+            }
 
-        for(let i= 0; i < cont; i++){
-            document.getElementById('pregunta_nueva_text' + i).value = array_preguntas[i];
-        }
-
-        cont++;
+            let select_preguntas = document.getElementById("select_preguntas");
+            
+            select_preguntas.innerHTML = '<option id=' + input + ' value=' + input + '>' + input + '</option>' + select_preguntas.innerHTML;
+            $('.form-select-label').selectpicker('refresh');
+            event.stopPropagation();
+        });
     }
 
-    document.getElementById("contador_nuevas").value = cont-1;
-}
-
-function getval(a)
-{
-    document.getElementById('pregunta_nueva_text'+a).value = document.getElementById('select_preguntas'+a).value;
-    document.getElementById('select_preguntas'+a).value = "Preguntas:";
-}
+});
