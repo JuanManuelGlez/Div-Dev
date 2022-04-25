12753ba10979811b42cca56fc8638b7e8901818a
A00572858 <A00572858@itesm.mx>
2022-04-25 16:47:20 -0500

    Puerto en base de datos

--- util/database.js
+++ util/database.js
@@ -20,7 +20,8 @@ const DB_DEV_EC = {
     host: 'ec2-34-214-126-148.us-west-2.compute.amazonaws.com',
     user: 'divdev',
     database: 'divdev',
-    password: 'Jorgito@22'
+    password: 'Jorgito@22',
+    port: 3306
 };
 
 const pool = mysql.createPool(DB_DEV_EC);
