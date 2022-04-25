b913e7392ea0c38169cc7618b6f935544f5301f0
A00572858 <A00572858@itesm.mx>
2022-04-25 16:30:20 -0500

    database change to instance

--- util/database.js
+++ util/database.js
@@ -16,6 +16,13 @@ const DB_DEV = {
     password: ''
 };
 
-const pool = mysql.createPool(DB_DEV);
+const DB_DEV_EC = {
+    host: 'ec2-34-214-126-148.us-west-2.compute.amazonaws.com',
+    user: 'divdev',
+    database: 'divdev',
+    password: 'Jorgito@22'
+};
+
+const pool = mysql.createPool(DB_DEV_EC);
 
 module.exports = pool.promise();
