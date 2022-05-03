2a331530afd0c5072a84a6d9853d3d55fa5f87e9
EmilianoV-TEC <a01707035@itesm.mx>
2022-05-03 07:47:10 -0800

    Agrega permisos admin privilegios

--- controllers/privilegios_controller.js
+++ controllers/privilegios_controller.js
@@ -3,28 +3,34 @@ const Privilegio = require("../models/privilegio");
 const Rol = require("../models/rol");
 
 exports.getRoles = (request, response, next) => {
-
-    Privilegio.fetchPrivilegios()
-                .then(([rowsPrivs, fieldDataPrivs]) => {
-                    Privilegio.fetchAll()
-                        .then(([rowsall, fieldData]) => {
-                            Rol.fetchAll()
-                                .then(([rows, fieldData]) => {
-                                    response.render('administrar_privilegios',{allprivilegios: rowsall, privilegiosact: rowsPrivs, roles: rows});
-                                })
-                                .catch(err => console.log(err));
-                        })
-                        .catch(err => console.log(err));
-                })
-                .catch(err => console.log(err));
+    if (request.session.privilegios.includes(12)) {
+        Privilegio.fetchPrivilegios()
+                    .then(([rowsPrivs, fieldDataPrivs]) => {
+                        Privilegio.fetchAll()
+                            .then(([rowsall, fieldData]) => {
+                                Rol.fetchAll()
+                                    .then(([rows, fieldData]) => {
+                                        response.render('administrar_privilegios',{allprivilegios: rowsall, privilegiosact: rowsPrivs, roles: rows});
+                                    })
+                                    .catch(err => console.log(err));
+                            })
+                            .catch(err => console.log(err));
+                    })
+                    .catch(err => console.log(err));
+    }
+    else {
+        response.redirect("/");
+    }
 }
 
 exports.EliminaPrivilegio = (request, response, next) => {
+    if (request.session.privilegios.includes(12)) {
     const id_rol = request.body.Rol;
     const id_priv = request.body.Id_Privilegio;
         Privilegio.EliminaPrivilegio(id_rol, id_priv)
             .then(response.status(200).json({}))
             .catch(err => console.log(err));
+    }
 }
 
 exports.AgregaPrivilegio = (request, response, next) => {
