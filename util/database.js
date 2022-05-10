9f67fc7cd36e362a6fe0d3da725aed51da4ef3bd
unknown <a01707439@itesm.mx>
2022-05-10 17:08:40 -0500

    merge

--- util/database.js
+++ util/database.js
@@ -24,10 +24,7 @@ const DB_DEV_EC = {
     port: 3306
 };
 
-<<<<<<< HEAD
+
 const pool = mysql.createPool(DB_DEV);
-=======
-const pool = mysql.createPool(DB_DEV_EC);
->>>>>>> 888210361ffe29fa00d54f64334b316b6e4fbc34
 
 module.exports = pool.promise();
